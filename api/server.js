a26972977c306cde4fd8d4109e020c07f9d049c8
yirano <yirano@protonmail.ch>
2020-07-22 00:44:51 -0400

    Commit MVP

--- api/server.js
+++ api/server.js
@@ -6,11 +6,69 @@ const server = express()
 
 server.use(express.json())
 
-server.get('/', async (req, res, next) => {
-  try {
 
+server.get('/accounts', async (req, res, next) => {
+  try {
+    const accounts = await db
+      .select('*')
+      .from('accounts')
+    res.json(accounts)
   } catch (error) {
     next(error)
+  }
+})
+
+server.get('/accounts/:id', async (req, res, next) => {
+  try {
+    const [account] = await db
+      .select('*')
+      .from('accounts')
+      .where('id', req.params.id)
+    res.json(account)
+  } catch (error) {
+    next(error)
+  }
+})
+
+server.post('/accounts', async (req, res, next) => {
+  try {
+    const newAcctID = await db
+      .insert(req.body)
+      .into('accounts')
+    const [newAcct] = await db
+      .select('*')
+      .from('accounts')
+      .where('id', newAcctID)
+    res.status(201).json(newAcct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+server.put('/accounts/:id', async (req, res, next) => {
+  try {
+    await db('accounts')
+      .update(req.body)
+      .where('id', req.params.id)
+
+    const editAcct = await db('accounts')
+      .select('*')
+      .where('id', req.params.id)
+
+    res.json(editAcct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+server.delete('/accounts/:id', async (req, res, next) => {
+  try {
+    await db('accounts')
+      .where('id', req.params.id)
+      .del()
+    res.status(204).end()
+  } catch (error) {
+    console.log(error)
   }
 })
 
